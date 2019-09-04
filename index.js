63d36b5a1a1f7992952a2a05f67ac849308f47b9
yasmeen1999 <yasmeenaleemkhan786@gmail.com>
2019-09-04 14:39:25 +0530

    2nd day practice

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-console.log("Hello world");
+//*variables*//
 
+
+
+
+let name = "yass";
+console.log(name);
+
+//rules to write a keyword
+//cannot be a  resurved keyword
+//should be meaningfull
+//cannot start with a number
+//cannot contain a space or hyphen
+
+
+let firstName = 'yass';
+let lastNmae= 'khan';
+
+console.log(firstName,lastNmae);
+
+//*constants*//
+let rate ='1.5';
+rate ='1';
+console.log(rate);
+//if we want to keep constant use const 
+
+
+//*primitives*//
+// 1 string
+// 2 numbers
+// 3 null
+// 4 undifined
+// 5 boolean
