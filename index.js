556816cc0d208969610d9f798e261e679dd87c52
yasmeen1999 <yasmeenaleemkhan786@gmail.com>
2019-11-11 22:57:44 +0530

    operators

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 //*variables*//
-
-
-
-
-let name = "yass";
-console.log(name);
+//let name = "yass";
+//console.log(name);
 
 //rules to write a keyword
 //cannot be a  resurved keyword
@@ -12,18 +8,16 @@ console.log(name);
 //cannot start with a number
 //cannot contain a space or hyphen
 
+//let firstName = "yass";
+//let lastNmae = "khan";
 
-let firstName = 'yass';
-let lastNmae= 'khan';
-
-console.log(firstName,lastNmae);
+//console.log(firstName, lastNmae);
 
 //*constants*//
-let rate ='1.5';
-rate ='1';
-console.log(rate);
-//if we want to keep constant use const 
-
+//const rate = "1.5";
+ //const rate1 = "1";
+//console.log(rate);
+//if we want to keep constant use const
 
 //*primitives*//
 // 1 string
@@ -31,3 +25,89 @@ console.log(rate);
 // 3 null
 // 4 undifined
 // 5 boolean
+
+//examples of primitive types
+//let age = 20; // number literal
+//let selectedcolor = null; // this type is used where we want to clear the value of a variable
+//let firstName = undifined;
+//let  isApproved = true; //boolean literals
+//console.log(age , selectedcolor , isApproved );
+
+
+//*object*//
+//let person = {
+  //  name : 'yasmeen',
+    //age: 20 ,
+    //address:'ramanagar'
+//};
+//dot notation
+
+//person.name='yass';
+
+//bracket notation
+//let selection ='name';
+//person['selection']='yasu';
+
+//console.log(person.name);
+
+//*array*//
+//let selectedColors =['red','blue'];
+//selectedColors[2] ='green';
+//console.log(selectedColors);
+
+//*functions*//
+//function greet (){
+   // console.log('hello world');
+//}
+
+//calling a function
+//greet();
+
+//*concatenateing two strings*//
+//function greet(name){
+
+  //  console.log('hello '  +  name );
+//}
+//greet('yass');
+//greet('khan');
+ 
+
+//*operators
+ //1. arthmetic
+ //2. assignment
+ //3.comparison
+ //4.logical
+ //5.bitwise
+
+
+ //*arthmetic*//
+// let X=10;
+// let Y=3;
+
+ //console.log(X/Y);
+// console.log(X*Y);
+ //console.log(X+Y);
+// console.log(X-Y);
+// console.log(X%Y);
+// console.log(X**Y);
+
+
+//*assignment operator*//
+//let X=10;
+//X +=5; //INCRIMENTING THIS BY 5//
+//X *=3;  //MULTIPLICATION 
+//console.log(X);
+
+//*COMPARISON OPERATOR*//
+let X=1;
+//console.log(X>1);
+//console.log(X>=1);
+//console.log(X<1);
+//console.log(X<=1);
+  /*equality operator*/
+
+ // console.log(X===1); EQUAL
+ // console.log(X!==1); NOT EQUAL
+
+
+ 
